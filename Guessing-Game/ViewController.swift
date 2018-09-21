78046827bc8dd308d2db4cbe71141e5cdef4a7b8
Katelyn Pace <katelynpace@Interapts-MacBook-Pro-4.local>
2018-09-21 10:57:05 -0500

    added the rest of the initial project files

---
+++ Guessing-Game/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Guessing-Game
+//
+//  Created by Katelyn Pace on 9/21/18.
+//  Copyright © 2018 Katelyn Pace. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
