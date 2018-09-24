60c9892a4ad38376cbf262e7fef014295ef907c6
Katelyn Pace <katelynpace@Interapts-MacBook-Pro-4.local>
2018-09-24 13:25:27 -0500

    Created the layout

--- Guessing-Game/ViewController.swift
+++ Guessing-Game/ViewController.swift
@@ -9,7 +9,21 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var GuessStatement: UILabel!
+    
+    @IBOutlet weak var EnterNumber: UITextField!
+    
+    @IBOutlet weak var GuessEval: UILabel!
+    
+    @IBOutlet weak var SubmitButton: UIButton!
+    
+    @IBOutlet weak var AttemptLevel: UILabel!
+    
+    @IBOutlet weak var GameReset: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
