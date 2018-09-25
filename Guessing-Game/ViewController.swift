36f9b9f52717597cd0eca6957f46980cb74ab552
Katelyn Pace <katelynpace@Interapts-MacBook-Pro-4.local>
2018-09-25 09:54:49 -0500

    Added reset button and score count

--- Guessing-Game/ViewController.swift
+++ Guessing-Game/ViewController.swift
@@ -12,8 +12,7 @@ class ViewController: UIViewController {
     
     
     @IBOutlet weak var GuessStatement: UILabel!
-    
-    @IBOutlet weak var EnterNumber: UITextField!
+
     
     @IBOutlet weak var GuessEval: UILabel!
     
@@ -21,14 +20,71 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var AttemptLevel: UILabel!
     
+    @IBOutlet weak var userGuess: UISlider!
+    
     @IBOutlet weak var GameReset: UIButton!
+
+    @IBOutlet weak var guessHint: UILabel!
+    
+    @IBOutlet weak var score: UILabel!
     
     
+    var randomNumber = Int(arc4random_uniform(99)) + 1
+    var numberOfTries = 10
+    var guessedNumber: Int?
+    var userScore = 0
+  
     override func viewDidLoad() {
+        
+        SubmitButton.setTitle("Submit", for: .normal)
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
 
+    
+  
+    @IBAction func sliderChanged(_ sender: Any) {
+        GuessEval.text = "\(Int(userGuess.value))"
+    }
+    
+    @IBAction func SubmitGuess(_ sender: Any) {
+        
+        if numberOfTries == 1 {
+            guessHint.text = "You ran out of tries!"
+            SubmitButton.isEnabled = false
+            AttemptLevel.text = "Attempts: 0"
+        }
+        else if Int(userGuess.value) == randomNumber {
+            guessHint.text = "You won!"
+            numberOfTries -= 1
+            AttemptLevel.text = "Attempts: 0"
+            SubmitButton.isEnabled = false
+            userScore += 1
+            score.text = "Win: \(userScore)"
+        }
+        else if Int(userGuess.value) < randomNumber {
+            guessHint.text = "Your guess was to low!"
+            numberOfTries -= 1
+            AttemptLevel.text = "Attempts: \(numberOfTries)"
+        }
+        else if Int(userGuess.value) > randomNumber {
+            guessHint.text = "Your guess was to high!"
+            numberOfTries -= 1
+            AttemptLevel.text = "Attempts: \(numberOfTries)"
+        }
+        }
+    
+    
+    @IBAction func resetGame(_ sender: Any) {
+        randomNumber = Int(arc4random_uniform(99)) + 1
+        numberOfTries = 5
+        AttemptLevel.text = "Attempts: \(numberOfTries)"
+        SubmitButton.isEnabled = true
+        guessHint.text = ""
+        userGuess.value = 1
+        GuessEval.text = "\(Int(userGuess.value))"
+        
+    }
+    
+    }
 
-}
 
